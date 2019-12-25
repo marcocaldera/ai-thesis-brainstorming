8467e9b3bb86539b70e02ec68db05b526b25054e
Marco Caldera <marco.caldera93@icloud.com>
2019-12-25 11:39:53 +0100

    [fix] catch exeception during execution

--- web-scraping/functions.py
+++ web-scraping/functions.py
@@ -18,7 +18,7 @@ def set_browser():
     options = webdriver.ChromeOptions()
     options.add_argument('headless')
     browser = webdriver.Chrome(executable_path='./chromedriver',
-                               # chrome_options=options
+                               chrome_options=options
                                )
     return browser
 
--- web-scraping/ig-post-navigation.py
+++ web-scraping/ig-post-navigation.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.keys import Keys
 from collections import Counter
 from functions import set_browser, login
 import pandas as pd
-import selenium
+from selenium.common.exceptions import NoSuchElementException
 
 
 def post(browser):
@@ -18,24 +18,31 @@ def post(browser):
     body = browser.find_element_by_tag_name("body")
 
     # click sulla prima foto
-    elem = browser.find_element_by_class_name("v1Nh3.kIKUG._bz0w").click()
-    time.sleep(0.4)
+    browser.find_element_by_class_name("v1Nh3.kIKUG._bz0w").click()
 
+    #info: prendere solo il primo commento se è dell'utente indicato
     user = []
     description = []
     published = []
-    for _ in range(50):
+    for i in range(50):
         try:
+            # print("qui", i)
+            time.sleep(0.6)
             text = browser.find_element_by_class_name('C4VMK').text.split('\n')
+            # print(text)
             user.append(text[0])
             description.append(text[1])
             published.append(text[2])
             body.send_keys(Keys.ARROW_RIGHT)
-            time.sleep(0.4)
-        except Exception as e:
-            print("troppo velocee")
-            time.sleep(0.4)
-            continue
+        except NoSuchElementException as e:
+            # print(e)
+            # print("qua", i)
+            user.append('ciao')
+            description.append('no description')
+            published.append('no')
+            body.send_keys(Keys.ARROW_RIGHT)
+            # time.sleep(0.4)
+            # continue
 
     print(description)
 
